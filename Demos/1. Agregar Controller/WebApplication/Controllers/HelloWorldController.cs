bac9bb1b52a7a1e7fa6f83a2afb47870be4b6d25
German Kuber <german.kuber@outlook.com>
2016-08-01 12:48:16 -0300

    First Commit

---
+++ Demos/1. Agregar Controller/WebApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "Hola mundo desde el Index";
+        }
+        public string Welcome()
+        {
+            return "Hola mundo desde la página de Welcome";
+        }
+        ///HelloWorld/WelcomeUser?user=
+        public string WelcomeUser(string user)
+        {
+            return $"Bienvenido {user} a la pagina de Welcome";
+        }
+        /////HelloWorld/WelcomeUser?user=
+        public string WelcomeData(string name, int id)
+        {
+            return $"Bienvenido {name} a la pagina de Welcome";
+        }
+    }
+}
